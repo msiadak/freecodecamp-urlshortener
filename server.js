d019eb94113030ccd9cc8ef47e3ec3dbe17959b1
Glitch (intelligent-meeting) <none>
2018-04-07 16:18:48 +0000

    📫💇 Checkpoint ./server.js:441208/55

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var cors = require('cors');
 
@@ -12,12 +13,13 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGOLAB_URI);
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+app.use(
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
