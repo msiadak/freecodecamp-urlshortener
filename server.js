79bdf85385206898756282c635670e4e99f07fca
Glitch (intelligent-meeting) <none>
2018-04-07 18:35:37 +0000

    👨‍❤️‍💋‍👨🌑 Checkpoint ./server.js:441208/712

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 'use strict';
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
+const dns = require('dns');
+const { URL } = require('url');
 
-var cors = require('cors');
+const express = require('express');
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-var app = express();
+const cors = require('cors');
+
+const app = express();
 
 // Basic Configuration 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
 mongoose.connect(process.env.MONGOLAB_URI);
@@ -19,7 +22,7 @@ app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
-app.use(bodyParser.urlEncoded({ extended: false });
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -29,8 +32,15 @@ app.get('/', function(req, res){
 
   
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.post("/api/shorturl/new", function (req, res) {
+  const url = req.body.url;
+  const hostname = new URL(url).hostname;
+  dns.lookup(hostname, err => {
+    if (err) {
+      return res.status(500).send(`Couldn't resolve hostname: ${url}`);
+    }
+    return res.json({ original_url: url, short_url: 1 });
+  });
 });
 
 
