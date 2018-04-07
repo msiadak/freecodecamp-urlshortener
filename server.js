65957ec5e5a90d417b2aac80a31612395396f7da
Glitch (intelligent-meeting) <none>
2018-04-07 18:46:34 +0000

    🏑🌓 Checkpoint ./server.js:441208/923

--- server.js
+++ server.js
@@ -12,35 +12,45 @@ const cors = require('cors');
 
 const app = express();
 
-// Basic Configuration 
 const port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
 mongoose.connect(process.env.MONGOLAB_URI);
 
+/*** DB object ***/
+const ShortURL = mongoose.model('ShortURL', new mongoose.Schema({
+  url: String,
+}));
+
+/*** Middleware ***/
 app.use(cors());
-
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function(req, res){
+/*** Handlers ***/
+app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
-app.post("/api/shorturl/new", function (req, res) {
+// Create new shortened URL
+app.post('/api/shorturl/new', (req, res) => {
   const url = req.body.url;
   const hostname = new URL(url).hostname;
   dns.lookup(hostname, err => {
     if (err) {
-      return res.status(500).send(`Couldn't resolve hostname: ${url}`);
+      return res.status(500).send(`Couldn't resolve hostname: ${hostname}`);
     }
+    ShortURL.create({ url }, (err, shortURL) => {
+      if (err) {
+        return res.status(500).send("Couldn't create
+      }
+    });
     return res.json({ original_url: url, short_url: 1 });
   });
+});
+
+// Retrieve shortened URL
+app.get('/api/shorturl/:id', (req, res) => {
+  
 });
 
 
